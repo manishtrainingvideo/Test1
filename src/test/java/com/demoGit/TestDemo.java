1daaa2bee3d51725f3f78e3faa258bde102b5343
manishtrainingvideo <manishtrainingvideo@gmail.com>
2020-02-07 07:22:53 +0530

    First Commit

---
+++ src/test/java/com/demoGit/TestDemo.java
@@ -0,0 +1,20 @@
+package com.demoGit;
+
+import org.testng.annotations.Test;
+
+public class TestDemo {
+
+	
+	@Test
+	public void firstTest(){
+		
+		System.out.println("This is first Test");
+	}
+	
+	@Test
+	public void secondTest(){
+		
+		System.out.println("This is SEcond Test");
+	}
+
+}
